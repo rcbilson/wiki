d8143066f879f952b91d9e3cec4182472febe9a2
Richard Bilson <rcbilson@gmail.com>
2025-05-12 16:39:33 -0400

    frontend: redirect / to /index

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import PageRenderer from './PageRenderer';
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/index" replace />} />
       <Route path=":page" element={<PageRenderer />} />
     </Routes>
   );
